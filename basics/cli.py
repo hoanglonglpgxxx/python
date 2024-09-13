1e80a8bcfcd046bcc9a7e6727917e66186472617
Mits <hoanglonglpgxxx@gmail.com>
2024-09-13 17:31:39 +0700

    init folder

---
+++ basics/cli.py
@@ -0,0 +1,10 @@
+user_prompt = "Enter a todo:"
+
+todo1 = input(user_prompt)#truyền vào string khởi đầu trước khi nhập
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1, todo2, todo3, "Hello"]
+
+print(todos)
+print(type(todos))
